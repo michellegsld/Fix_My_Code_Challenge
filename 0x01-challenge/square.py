244c5d6a5bff883d6c445ff1b614bb57a6129f7b
Michelle Giraldo <michellegsld@gmail.com>
2020-05-19 14:16:08 -0400

    [UNDO] Revert last commit

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -8,6 +8,7 @@ class Square():
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """ Set square attributes """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
