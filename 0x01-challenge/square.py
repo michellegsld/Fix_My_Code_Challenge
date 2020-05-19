d1f02ed95c3dc7d7c898a2379227fee748951414
Michelle Giraldo <michellegsld@gmail.com>
2020-05-19 13:37:55 -0400

    [Fix] Perimeter was spelt incorrectly

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -16,7 +16,7 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def PerimeterOfMySquare(self):
         """ Perimeter of a Square """
         return (self.width * 2) + (self.height * 2)
 
@@ -30,4 +30,4 @@ if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.PerimeterOfMySquare())
