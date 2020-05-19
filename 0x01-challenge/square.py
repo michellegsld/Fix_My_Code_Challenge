7b8385155379479feba0455b40074d63b659749c
Michelle Giraldo <michellegsld@gmail.com>
2020-05-19 14:05:10 -0400

    [Update] Add setters and getters

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -7,20 +7,40 @@ class square():
 
     def __init__(self, width, height):
         """ Set square attributes """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ return width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ set width """
+        self.__width = value
+
+    @property
+    def height(self):
+        """ return height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ set height """
+        self.__height = value
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.__width * self.__height
+        return self.width * self.height
 
     def perimeter_of_my_square(self):
         """ Perimeter of a Square """
-        return (self.__width * 2) + (self.__height * 2)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """ String version of a square """
-        return "{}/{}".format(self.__width, self.__height)
+        return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
