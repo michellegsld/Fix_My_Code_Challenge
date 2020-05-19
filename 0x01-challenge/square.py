85a9f6f6d5724fec085d5e9f4c49609f8906b2af
Michelle Giraldo <michellegsld@gmail.com>
2020-05-19 13:31:10 -0400

    [Update] Undo last commit and add comments everywhere needed

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+""" Contains a class to create a square """
 
 class square():
+    """ Initialize these attributes to 0 """
     width = 0
     height = 0
 
 
     def __init__(self, *args, **kwargs):
+        """ Set square attributes """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -14,14 +17,17 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Perimeter of a Square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ String version of a square """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    s = square(width=12, height=12)
+    """ Create a square if this file was executed """
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
